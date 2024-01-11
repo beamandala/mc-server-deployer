d09c1220fda2db97ac1b804524d12b4ac7b5f457
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2024-01-10 18:05:17 -0600

    Finished moving related routes to user.rs file

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,10 @@
-mod get_user;
+mod user;
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use aws_config::{profile::Property, BehaviorVersion};
+use actix_web::{web, App, HttpServer};
+use aws_config::BehaviorVersion;
 use aws_sdk_dynamodb as dynamodb;
-use dynamodb::types::AttributeValue;
-use get_user::user;
-use serde::{Deserialize, Serialize};
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-};
+use std::sync::{Arc, Mutex};
+use user::{create_user, get_user};
 
 #[derive(Clone)]
 pub struct AppState {
@@ -36,98 +31,5 @@ async fn main() -> std::io::Result<()> {
 }
 
 fn app_config(config: &mut web::ServiceConfig) {
-    config.service(web::scope("").service(new_user).service(user));
+    config.service(web::scope("").service(create_user).service(get_user));
 }
-
-#[derive(Deserialize)]
-struct NewUserRequestData {
-    email: String,
-    name: String,
-}
-
-#[post("/new-user")]
-async fn new_user(
-    data: web::Data<AppState>,
-    properties: web::Json<NewUserRequestData>,
-) -> impl Responder {
-    let db = data.db_client.lock().unwrap();
-
-    let mut user_item: HashMap<String, AttributeValue> = HashMap::new();
-    user_item.insert(
-        "email".to_string(),
-        AttributeValue::S(properties.email.to_owned()),
-    );
-    user_item.insert(
-        "name".to_string(),
-        AttributeValue::S(properties.name.to_owned()),
-    );
-
-    let db_res = db
-        .put_item()
-        .table_name("mine-auth")
-        .set_item(Some(user_item))
-        .send()
-        .await;
-
-    match db_res {
-        Ok(o) => {
-            println!("{:?}", o);
-
-            return HttpResponse::Ok().body(format!("{:?}", o));
-        }
-        Err(e) => {
-            println!("{:?}", e);
-
-            return HttpResponse::InternalServerError().body(format!("{:?}", e));
-        }
-    }
-}
-
-#[derive(Deserialize)]
-struct GetUserRequestData {
-    email: String,
-}
-
-// #[derive(Serialize, Deserialize, Debug)]
-// struct GetUserResponseData {
-//     item: Option<UserDetails>,
-// }
-//
-// #[derive(Deserialize, Debug)]
-// struct UserDetails {
-//     email: AttributeValue,
-//     name: AttributeValue,
-// }
-
-// #[get("/user")]
-// async fn user(
-//     data: web::Data<AppState>,
-//     properties: web::Json<NewUserRequestData>,
-// ) -> impl Responder {
-//     let db = data.db_client.lock().unwrap();
-//
-//     let get_user_res = db
-//         .get_item()
-//         .table_name("mine-auth")
-//         .key("email", AttributeValue::S(properties.email.to_owned()))
-//         .send()
-//         .await;
-//
-//     match get_user_res {
-//         Ok(res) => {
-//             println!("{:?}", res);
-//
-//             if let item = Some(res.item) {
-//                 // let user: GetUserResponseData = serde_json::from_str(item).unwrap();
-//                 // return HttpResponse::Ok(web::Json(user));
-//             }
-//         }
-//         Err(e) => {
-//             println!("{:?}", e);
-//
-//             return HttpResponse::InternalServerError();
-//         }
-//     }
-//
-//     HttpResponse::Ok()
-// }
--- backend/src/user.rs
+++ backend/src/user.rs
@@ -1,10 +1,11 @@
 use crate::AppState;
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{get, post, web, HttpResponse, Responder};
 use aws_sdk_dynamodb::types::AttributeValue;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 #[derive(Deserialize)]
-struct NewUserRequestData {
+struct GetUserRequestData {
     email: String,
 }
 
@@ -20,7 +21,7 @@ struct NewUserRequestData {
 // }
 
 #[get("/user")]
-pub async fn user(
+pub async fn get_user(
     data: web::Data<AppState>,
     properties: web::Json<NewUserRequestData>,
 ) -> impl Responder {
@@ -50,4 +51,48 @@ pub async fn user(
     }
 
     HttpResponse::Ok()
+}
+
+#[derive(Deserialize)]
+struct NewUserRequestData {
+    email: String,
+    name: String,
+}
+
+#[post("/new-user")]
+pub async fn create_user(
+    data: web::Data<AppState>,
+    properties: web::Json<NewUserRequestData>,
+) -> impl Responder {
+    let db = data.db_client.lock().unwrap();
+
+    let mut user_item: HashMap<String, AttributeValue> = HashMap::new();
+    user_item.insert(
+        "email".to_string(),
+        AttributeValue::S(properties.email.to_owned()),
+    );
+    user_item.insert(
+        "name".to_string(),
+        AttributeValue::S(properties.name.to_owned()),
+    );
+
+    let db_res = db
+        .put_item()
+        .table_name("mine-auth")
+        .set_item(Some(user_item))
+        .send()
+        .await;
+
+    match db_res {
+        Ok(o) => {
+            println!("{:?}", o);
+
+            return HttpResponse::Ok().body(format!("{:?}", o));
+        }
+        Err(e) => {
+            println!("{:?}", e);
+
+            return HttpResponse::InternalServerError().body(format!("{:?}", e));
+        }
+    }
 }
